9597ffbfef8a9c05d867ef32a5c93fb7b4cbb923
atfb84 <atfb84@mst.edu>
2023-03-17 17:50:53 -0700

    File structure

---
+++ app.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from credentials import (
+    test_user
+)
+
+
+chrome = webdriver.Chrome(executable_path="C:\\Users\\afore\\chromedriver_win32\\chromedriver.exe")
