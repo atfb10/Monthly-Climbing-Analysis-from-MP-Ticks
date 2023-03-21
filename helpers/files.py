79b2cce55fcd99a440b86315ac4e6acb7bda1b46
atfb84 <atfb84@mst.edu>
2023-03-20 19:43:56 -0700

    Move user csv file and make user file folder functions

--- helpers/files.py
+++ helpers/files.py
@@ -3,6 +3,7 @@ Author: Adam Forestier
 Date: March 20, 2023
 Description: files contains various functions regarding files
 '''
+import os
 import pandas as pd
 
 from users.user import MpUser
@@ -17,8 +18,24 @@ def get_user_ticks(user: MpUser) -> None:
     df.to_csv(user.csv_filename, index=False)
     return
 
-# TODO: Create function to remove user files from within their directory prior to adding the new ones each month
+def move_user_csv(user: MpUser) -> None:
+    '''
+    arguments: an MpUser object
+    returns: None
+    description: move_user_csv() moves the csv files in the parent directory into a users subfolder under the user_files directory
+    '''
+    new_path = f'files/{user.username}/{user.csv_filename}'
+    os.rename(user.csv_filename, new_path)
+    return
 
-# TODO: Add user files to their respective folder after they have been generated for the month & the previous months have been removed
-
-# TODO: Create user folder within files directory whenever a new user is added to the database
+def make_user_files_folder(user: MpUser):
+    '''
+    arguments: an MpUser object
+    returns: none
+    description: make_user_files_folder creates a folder under the user_files directory for each user that will store their data files
+    '''
+    user_folder = str(user.username)
+    parent_dir = "/user_files/"
+    path = os.path.join(parent_dir, user_folder)
+    os.mkdir(path)
+    return
