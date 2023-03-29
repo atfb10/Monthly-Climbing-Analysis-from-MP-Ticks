1e045c5621450faba742ab092902617fb167d44b
atfb84 <atfb84@mst.edu>
2023-03-29 09:28:51 -0700

    Implement scheduling for the 1st of every month

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ author: Adam Forestier
 date: March 28, 2023
 description: app.py manages function calls and object creation to run the project
 '''
+from datetime import date
+import schedule
 
 from database.mp_db import get_users
 from helpers.email import send_mail
@@ -12,12 +14,25 @@ from helpers.files import (
     )
 from users.user import MpUser
 
-# Run the Application
-users = [MpUser(user[0], user[1], user[2]) for user in get_users()]
-dataframes = []
-[dataframes.append(get_user_ticks(user)) for user in users]
-[zip_user_folder(user) for user in users]
-[send_mail(user) for user in users]
+def job() -> None:
+    '''
+    arguments: 
+    returns: None
+    description: job() runs all functions to make the app perform intended functionality. But only if the day is the first of the month
+    '''
+    if date.today().day != 1:
+        return
+    users = [MpUser(user[0], user[1], user[2]) for user in get_users()]
+    dataframes = []
+    [dataframes.append(get_user_ticks(user)) for user in users]
+    [zip_user_folder(user) for user in users]
+    [send_mail(user) for user in users]
+
+schedule.every().day.at('06:00').do(job)
+
+while True:
+    schedule.run_pending()
+
 
 # TODO: REMAINING STEPS
 # 1. data
