47ff90656cd1a9b6f2687594cb9a244a1f5e6f69
atfb84 <atfb84@mst.edu>
2023-03-31 15:02:56 -0700

    lots of stats - like a boss

--- data/statistics.py
+++ data/statistics.py
@@ -6,9 +6,14 @@ Description: statistics.py contains the MpUserStatistics class
 
 import numpy as np
 import pandas as pd
+
+from data.data import (
+    ROCK_CLIMBING_GRADES,
+    ROCK_CLIMBING_GRADES_TO_NUMERIC
+)
 from users.user import MpUser
 
-class PlotlyGraphMpUserStatistics:
+class MpUserStatistics:
     '''
     objects of the PlotlyGraph class contains: 
     parameters: a user object
@@ -23,4 +28,85 @@ class PlotlyGraphMpUserStatistics:
         returns: none
         description: stats() gathers statistical information and writes it a csv file
         '''
+        hardest_attempt_string = self.__hardest_route_attempt()
+        hardest_send_string = self.__hardest_route_sent()
+        total_feet_climbed = self.__total_feet_climbed()
         return
+    
+    def __assign_climbing_grade_numeric(self, grade: str, climbing_grade_dict: dict = ROCK_CLIMBING_GRADES_TO_NUMERIC) -> int:
+        '''
+        arguments: self, grade, dictionary of climbing grades assigned to numeric
+        returns: an integer value associated with a climbing grade
+        description: __assign_climbing_grade_numeric utilizes a dicionary to associate a Yosemite Decimal System climbing with an integer 
+        '''
+        for key, value in climbing_grade_dict.items():
+            if key == grade:
+                return value
+        return -1 # Something went wrong
+    
+    def __order_by_climbing_grades(self) -> pd.DataFrame:
+        '''
+        arguments: self
+        returns: ordered dataframe with only rock climbs
+        description: __order_by_climbing_grades orders a dataframe by climbing difficulty in ascending order
+        '''
+        df = self.__only_rock_climbing()
+        df['Numeric Grade'] = np.vectorize(self.__assign_climbing_grade_numeric)(df['Rating'])
+        df = df.sort_values(by=['Numeric Grade'])
+        return df
+    
+    def __only_rock_climbing(self) -> pd.DataFrame:
+        '''
+        arguments: self
+        returns: ordered dataframe with only rock climbs
+        description: __only_rock_climbing returns a dataframe with only rock climbs
+        '''
+        df = self.user.df
+        df = df[df['Rating'].isin(ROCK_CLIMBING_GRADES)]
+        return df
+    
+    def __only_sends_rock_climbing(self) -> pd.DataFrame:
+        '''
+        arguments: self
+        returns: dataframe with only sends while roped
+        description: __only_sends_rock_climbing filters out unsent lead climbing
+        '''
+        df = self.__order_by_climbing_grades()
+        df = df[df['Lead Style'].isin(['Onsight', 'Flash', 'Redpoint'])]
+        return df
+    
+    def __hardest_route_attempt(self) -> str:
+        '''
+        arguments: self
+        returns: dictionary of route name and its grade
+        description: __highest_climbing_send returns a tuple with hardest route attempted 
+        '''
+        df = self.__order_by_climbing_grades()
+        num_rows = len(df) - 1
+        hardest_route = df.iloc[num_rows,]
+        route_name = hardest_route['Route']
+        route_grade = hardest_route['Rating']
+        return f'Hardest Route Attempted - {route_name}: {route_grade}'
+
+    def __hardest_route_sent(self) -> str:
+        '''
+        arguments: self
+        returns: dictionary of route name and its grade
+        description: __highest_climbing_send returns a tuple with hardest route attempted 
+        '''
+        df = self.__only_sends_rock_climbing()
+        num_rows = len(df) - 1
+        hardest_route = df.iloc[num_rows,]
+        route_name = hardest_route['Route']
+        route_grade = hardest_route['Rating']
+        return f'Hardest Send - {route_name}: {route_grade}'
+    
+    def __total_feet_climbed(self):
+        '''
+        arguments: self
+        returns: string of feet climbed
+        description: __total_feet_climbed returns total feet climbed as a string
+        '''
+        df = self.user.df
+        feet = df['Length'].sum()
+        return str(f'Total Feet Climbed: {feet}')
