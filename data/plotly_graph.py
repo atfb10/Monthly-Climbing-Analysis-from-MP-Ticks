9a99a9636f5a0fba59ed26dac61c18dbc0eed301
atfb84 <atfb84@mst.edu>
2023-03-31 10:02:23 -0700

    Lead style count graph

--- data/plotly_graph.py
+++ data/plotly_graph.py
@@ -6,6 +6,7 @@ Description: plotly_graph.py contains the PlotlyGraph class
 
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import plotly.offline as pyo
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
@@ -30,17 +31,23 @@ class PlotlyGraph:
         description: graph() calls the graphing methods of self
         '''
         self.distrubtion_of_quality_routes()
+        self.lead_style_count_graphed()
         return
     
-    def move_graph_to_user_folder(self, filename):
-        old_path = f'{PATH_TO_PROJECT}\\{filename}'
+    def move_graph_to_user_folder(self, filename) -> None:
+        '''
+        arguments: self, name of file to move
+        returns: none
+
+        '''
+        old_path = f'{PATH_TO_PROJECT}\\{filename}' 
         new_path = f'{PATH_TO_PROJECT}\\user_files\\{self.user.username}\\{filename}'
         if os.path.isfile(new_path):
             os.remove(new_path)
         os.rename(old_path, new_path)
         return
     
-    def distrubtion_of_quality_routes(self):
+    def distrubtion_of_quality_routes(self) -> None:
         '''
         arguments: self
         returns: None
@@ -56,6 +63,31 @@ class PlotlyGraph:
             yaxis_title='Density'
             )
         filename = 'Distribution of Route Quality.html'
+        pyo.plot(fig, filename=filename)
+        self.move_graph_to_user_folder(filename)
+        return
+    
+    def change_to_tr_if_no_lead(self, style: str) -> str:
+        '''
+        arguments: self
+        returns: original string if not "Not Led", else returns Toprope
+        description: changes "Not Led" to toprope
+        '''
+        if style == 'Not Led':
+            style = 'Toprope'
+        return style
+    
+    def lead_style_count_graphed(self):
+        '''
+        arguments: self
+        returns: None
+        description: countplot of number of each leadstyle
+        '''
+        df = self.user.df
+        df = df[df['Route Type'] != 'Boulder']
+        df['Lead Style'] = np.vectorize(self.change_to_tr_if_no_lead)(df['Lead Style'])
+        fig = px.histogram(df, x='Lead Style', color='Lead Style', title='Roped Climb Style Count', text_auto=True)
+        filename =  'Roped Climb Style Count.html'
         pyo.plot(fig, filename=filename)
         self.move_graph_to_user_folder(filename)
         return
