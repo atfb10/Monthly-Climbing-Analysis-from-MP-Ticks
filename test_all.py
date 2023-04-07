84f31da7606bcb7666fed9d425e8c7726cac1561
atfb84 <atfb84@mst.edu>
2023-04-07 11:25:18 -0700

    Create a test file to test stats and graphs without waiting for first of the month

--- test_all.py
+++ test_all.py
@@ -1,20 +1,22 @@
-'''
-author: Adam Forestier
-date: March 31, 2023
-description: test_stats.py tests is here to test the app's statistics functionality without waiting for the scheduler
-'''
-
 from database.mp_db import get_users
 from helpers.email import send_mail
 from helpers.files import (
     get_user_ticks,
     zip_user_folder
     )
+from data.plotly_graph import PlotlyGraph
 from data.statistics import MpUserStatistics
 from users.user import MpUser
 
 users = [MpUser(user[0], user[1], user[2]) for user in get_users()]
 [get_user_ticks(user) for user in users]
-justin = users[2]
-justin_stats = MpUserStatistics(justin)
-justin_stats.stats()
+adam = users[0]
+adam_stats = MpUserStatistics(adam)
+adam_graph = PlotlyGraph(adam)
+adam_stats.stats()
+adam_graph.graph()
+
+# [zip_user_folder(user) for user in users]
+# [send_mail(user) for user in users]
+zip_user_folder(adam)
+send_mail(adam)
