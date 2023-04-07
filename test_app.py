bb97998ee519b26a2400f13735bcdd71267f1d72
atfb84 <atfb84@mst.edu>
2023-04-07 12:32:32 -0700

    Final test for single user

---
+++ test_app.py
@@ -0,0 +1,27 @@
+'''
+author: Adam Forestier
+date: April 7, 2023
+description: test_app.py tests the app's graphing and statistical functionality without waiting for the scheduler
+'''
+
+from database.mp_db import get_users
+from helpers.email import send_mail
+from helpers.files import (
+    get_user_ticks,
+    zip_user_folder
+    )
+from data.plotly_graph import PlotlyGraph
+from data.statistics import MpUserStatistics
+from users.user import MpUser
+
+# Create objects
+users = [MpUser(user[0], user[1], user[2]) for user in get_users()]
+[get_user_ticks(user) for user in users]
+users_stats = [MpUserStatistics(user) for user in users]
+users_graphs = [PlotlyGraph(user) for user in users]
+
+# Get stats, graph, zip and send email for objects
+[user.stats() for user in users_stats]
+[user.graph() for user in users_graphs]
+[zip_user_folder(user) for user in users]
+# [send_mail(user) for user in users]
